ba45002b873553fe2f101e4941f96487e673b7aa
syuchan1005 <syuchan.dev@gmail.com>
2020-06-11 19:23:33 +0900

    :construction: Add useValidationState hook

--- src/client/hooks/useValidationState.ts
+++ src/client/hooks/useValidationState.ts
@@ -1,9 +1,15 @@
 import { useEffect, useMemo, useState } from 'react';
 
-type ValidateFunc<T> = (v: T) => string | undefined;
+type ValidateResult = string | { [key: string]: string | undefined } | undefined;
+type ValidateFunc<T> = (v: T) => ValidateResult;
 
 export const ValidateOperations: { [key: string]: () => ValidateFunc<any> } = {
   required: <T>(): ValidateFunc<T> => (v: T) => (v ? undefined : 'Required'),
+  objectRequired: <T = { [key: string]: any }>(): ValidateFunc<T> => (v: T) => Object.entries(v)
+    .reduce((obj, [key, value]) => ({
+      ...obj,
+      [key]: (value ? undefined : 'Required'),
+    }), {}),
 };
 
 // eslint-disable-next-line import/prefer-default-export
@@ -11,12 +17,8 @@ export const useValidationState = <T>(
   initVal: T,
   validationList: Array<ValidateFunc<T>>,
   enableError: boolean | undefined = true,
-): [T, (v: T) => void, string | undefined, boolean] => {
+): [T, (v: T) => void, ValidateResult, boolean] => {
   const [value, setValue] = useState<T>(initVal);
-
-  useEffect(() => () => {
-    setValue(initVal);
-  }, [setValue, initVal]);
 
   const [error, hasError] = useMemo(() => {
     // eslint-disable-next-line no-restricted-syntax
